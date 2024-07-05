0b4e97266353caf61e790c8465623c333f9b0b5d
srujan0404 <srujan.23bcs10107@ms.sst.scaler.com>
2024-07-05 10:30:45 +0530

    added all files

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,21 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link" 
+
+const Landing = () => {
+    return (
+      <div>
+        Landing Page (Unprotected)
+        <div className="flex flex-row ">
+          <Link href="/sign-in">
+            <Button className="w-56 rounded-md ml-5">Login</Button>
+          </Link>
+
+          <Link href="/sign-up">
+            <Button className="w-56 rounded-md ml-3">Register</Button>
+          </Link>
+        </div>
+      </div>
+    );
+} 
+
+export default Landing;
